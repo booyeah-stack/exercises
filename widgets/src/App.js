0b844993c48416756cc8ba6a1bc46dc441dccbce
Krasen Hristov <krsnhrstv@gmail.com>
2021-02-25 13:20:44 +0000

    we finished the translation widget, used everything we learned so far

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./Components/Accordion";
 import Search from "./Components/Search";
 import Dropdown from "./Components/Dropdown";
-
+import Translate from "./Components/Translate";
 const items = [
   {
     title: "What is React?",
@@ -34,21 +34,9 @@ const options = [
 ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
-
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>
-        Toggle dropdown
-      </button>
-      {showDropdown ? (
-        <Dropdown
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={options}
-        />
-      ) : null}
+      <Translate />
     </div>
   );
 };
